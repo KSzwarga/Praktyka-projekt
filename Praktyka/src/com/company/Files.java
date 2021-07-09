fcab1de97dc23f8a58f44987e7440b1a094aa937
Szwarga Kacper <kacper.szwarga@asseco.pl>
2021-07-09 09:25:41 +0200

    initial commit

---
+++ Praktyka/src/com/company/Files.java
@@ -0,0 +1,108 @@
+package com.company;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class Files {
+
+    String dir;
+
+    public Files(String directory){
+        dir =  directory + "\\";
+    }
+
+    public boolean exists(String path){
+        if (new File(dir + path).isFile()) {
+            System.out.println("File:" + path + " exists");
+            return true;
+        }
+        else {
+            System.out.println("File:" + path + "does not exist");
+            return false;
+        }
+    }
+
+    public void info(String path) {
+        File file = new File(dir + path);
+        if (file.isFile()){
+            System.out.println("File name:" + file.getName());
+            System.out.println("File path:" + file.getAbsolutePath());
+            System.out.println("File size:" + file.length() +" bytes");
+            System.out.println("File last time modified:" + new Date(file.lastModified())+"\n");
+        }
+        else {
+            System.out.println("File doesnt exist.");
+        }
+    }
+
+    public void info(String dateFrom, String dateTo) {
+        try {
+            Date date1 = new SimpleDateFormat("dd/MM/yyyy").parse(dateFrom);
+            Date date2 = new SimpleDateFormat("dd/MM/yyyy").parse(dateTo);
+
+            FileFilter fileFilter = file -> {
+                if (!file.isFile()) return false;
+                Date lastModified = new Date(file.lastModified());
+                return lastModified.after(date1) && lastModified.before(date2);
+            };
+
+            File directory = new File(dir);
+            File[] files = directory.listFiles(fileFilter);
+            assert files != null;
+            for (File file : files) {
+                try{
+                    System.out.println("File name:" + file.getName());
+                    System.out.println("File path:" + file.getAbsolutePath());
+                    System.out.println("File size:" + file.length() +" bytes");
+                    System.out.println("File last time modified:" + new Date(file.lastModified()) +"\n");
+                } catch(Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createFile(String path, String Content) {
+        try {
+            File newFile = new File(dir + path);
+            if (newFile.createNewFile()) {
+                System.out.println("File created.");
+                PrintWriter printWriter = new PrintWriter(dir + path, StandardCharsets.UTF_8);
+                printWriter.write(Content);
+                printWriter.close();
+            } else {
+                System.out.println("File already exists.");
+            }
+
+        }
+         catch (IOException e) {
+             System.out.println("An error occurred while creating or writing to a file.");
+             e.printStackTrace();
+        }
+
+
+    }
+
+    public void readFile(String path) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(dir + path));
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        }
+        catch (IOException e){
+            System.out.println("Error occured while trying to read a file");
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ Praktyka/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Files dataBase = new Files("C:\\Users\\kacper.szwarga\\Desktop\\BazaDanych");
+        dataBase.createFile("Test.txt","Some content");
+        dataBase.exists("Test.txt");
+        dataBase.info("Test.txt");
+        dataBase.readFile("Test.txt");
+        dataBase.info("07/06/2021","10/07/2021");
+    }
+}
